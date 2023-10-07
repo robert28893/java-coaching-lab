7cafed6a2981798702d198719c7be468ad99291a
robert <phuonghoanganh@admicro.vn>
2023-10-07 19:45:17 +0700

    add ci-build.yml

--- source/sample-project/src/test/java/vn/unigap/java/api/controller/UserControllerTest.java
+++ source/sample-project/src/test/java/vn/unigap/java/api/controller/UserControllerTest.java
@@ -3,26 +3,29 @@ package vn.unigap.java.api.controller;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.util.UriComponentsBuilder;
+import vn.unigap.java.api.dto.in.AuthLoginDtoIn;
 import vn.unigap.java.api.dto.in.PageDtoIn;
+import vn.unigap.java.api.dto.out.AuthLoginDtoOut;
 import vn.unigap.java.api.dto.out.PageDtoOut;
 import vn.unigap.java.api.dto.out.UserDtoOut;
-import vn.unigap.java.api.service.SampleDataGenerationService;
 import vn.unigap.java.common.response.ApiResponse;
 
 @SpringBootTest()
 @AutoConfigureMockMvc
 //@WebMvcTest(controllers = {UserController.class})
-@MockBean(classes = {SampleDataGenerationService.class})
+//@MockBean(classes = {SampleDataGenerationService.class})
 class UserControllerTest {
 
 //    @Value(value="${local.server.port}")
@@ -38,6 +41,32 @@ class UserControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    private String accessToken;
+
+    @BeforeEach
+    void getAccessToken() throws Exception {
+        var uri = UriComponentsBuilder.fromUriString("/auth/login")
+                .toUriString();
+
+        AuthLoginDtoIn loginDtoIn = new AuthLoginDtoIn();
+        loginDtoIn.setUsername("user");
+        loginDtoIn.setPassword("password");
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post(uri)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsBytes(loginDtoIn))
+                ).andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(result -> {
+                    var response = objectMapper.readValue(result.getResponse().getContentAsString(),
+                            new TypeReference<ApiResponse<AuthLoginDtoOut>>() {
+                            }
+                    );
+                    accessToken = response.getObject().getAccessToken();
+                });
+    }
 
     @Test
     void list() throws Exception {
@@ -59,7 +88,11 @@ class UserControllerTest {
                 .queryParam("pageSize", pageDtoIn.getPageSize())
                 .toUriString();
 
-        mockMvc.perform(MockMvcRequestBuilders.get(uri)).andDo(MockMvcResultHandlers.print())
+        System.out.println("ACCESS_TOKEN: " + accessToken);
+
+        mockMvc.perform(MockMvcRequestBuilders.get(uri)
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken))
+                .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk()).andExpect(result -> {
                     var response = objectMapper.readValue(result.getResponse().getContentAsString(),
                             new TypeReference<ApiResponse<PageDtoOut<UserDtoOut>>>() {
